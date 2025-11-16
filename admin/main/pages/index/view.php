f22024f86dc82717cc459fff2097e1643700e1cf
Srikee <kee19384@gmail.com>
2025-11-16 15:45:27 +0700

    ปรับเพิ่ม qrcode ไลน์ลูกค้าในหน้าแรก

--- admin/main/pages/index/view.php
+++ admin/main/pages/index/view.php
@@ -10,8 +10,13 @@
         <?php
             // Func::PrintData($_SESSION);
         ?>
-        <h4>ยินดีต้อนรับ</h4>
-        <h6>เข้าระบบโครงการตานีแก้จน</h6>
-        <img src="../../images/lineoa.png" alt="Line OA">
+        <!-- <h4>ยินดีต้อนรับ</h4>
+        <h6>เข้าระบบโครงการตานีแก้จน</h6> -->
+        <div class="row text-center">
+            <div class="col-sm-4 col-md-3">
+                <img src="../../images/lineoa.png" alt="Line OA" class="w-100">
+                <div class="fs-5">ไลน์ลูกค้า</div>
+            </div>
+        </div>
     </div>
 </div>
